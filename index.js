bd99f824239169261727cef8245db6d025c2e6f2
hewman82 <138827425+hewman82@users.noreply.github.com>
2023-10-02 22:53:24 -0700

    comments

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+// Import dependencies, shape classes, generateSVG function
 const fs = require('fs');
 const inquirer = require('inquirer');
 const { Triangle, Circle, Square } = require('./lib/shapes');
-
 const generateSVG = require('./lib/generatesvg.js');
 
+// Inquirer prompt questions
 const questions = [ {
     type: 'input',
     name: 'text',
@@ -24,7 +25,8 @@ const questions = [ {
 } ];
 
 function writeToFile(fileName, data) {
-    // Use write file method with filename and data parameters
+    
+    // Use write file method
     fs.writeFile(fileName, generateSVG(data), (err) => 
         err ? console.error(err) : console.log('Logo generated!')
     );
@@ -33,13 +35,17 @@ function writeToFile(fileName, data) {
 function init() {
     inquirer.prompt(questions)
     .then ((response) => {
+
+        // Create new class based on chosen shape
         if(`${response.shape}` === "Triangle") {
+            // New constructor class with parameters from prompt responses
             var shape = new Triangle(response.color, response.text, response.textColor);
         } else if(`${response.shape}` === 'Circle') {
             var shape = new Circle(response.color, response.text, response.textColor);
         } else if (`${response.shape}` === 'Square') {
             var shape = new Square(response.color, response.text, response.textColor);
         }
+        // Call writeToFile function
         writeToFile('logo.svg', shape.render(shape.dimensions, shape.setColor(shape.color), shape.textScript));
     });
 }
